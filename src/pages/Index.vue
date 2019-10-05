271c602bc7011a2ffcb147a96d08f0cb0b27243a
geedix <geedix@comcast.net>
2019-10-05 07:53:11 -0400

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,52 @@
+<template>
+  <Layout>
+
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Today is a good day!<br>Brad and Mary and Jack! ! only if you're in gridsome develop, it's instant.</h1>
+
+    <p>
+      It's the pages/index.vue file that came with gridsome. <br> 
+      refreshes without a refresh! So it just Freshes.
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+
+
+
+
+
+
+
+    
+
+  </Layout>
+
+
+
+
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+
+
+
+
+
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
---
+++ src/pages/Notes.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <h1>Notes</h1>
+    <p>
+      Everyone has a note. We have several.
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Notes'
+  }
+}
+</script>
